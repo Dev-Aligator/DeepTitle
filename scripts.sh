acbdcb2c65b071283c4991b2009d35a572607986
Dev-Aligator <devaligator1004@gmail.com>
2023-10-02 20:59:40 +0700

    Im tired of this

--- scripts.sh
+++ scripts.sh
@@ -1,4 +1,7 @@
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+#!/bin/bash
+
+# Get the absolute path to the directory containing this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Run main.py using the absolute path
 python "$SCRIPT_DIR/main.py" "$@"
